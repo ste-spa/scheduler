db5c06b825966a588f7b11b3e583c13c91d23124
ste-spa <129418513+ste-spa@users.noreply.github.com>
2023-04-19 20:57:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Get HTML elements
-const algorithmsContainer = document.querySelectorAll('input[type="radio"][name="algorithm"]');
-const tasksContainer = document.getElementById('tasks-container');
-const statisticsContainer = document.getElementById('statistics-container');
-const generateTasksButton = document.getElementById('generate-tasks');
-const startButton = document.getElementById('start');
-const resetButton = document.getElementById('reset');
-const continuousCheckbox = document.getElementById('continuous');
+const algorithmsContainer = document.querySelectorAll(
+  'input[type="radio"][name="algorithm"]'
+);
+const tasksContainer = document.getElementById("tasks-container");
+const statisticsContainer = document.getElementById("statistics-container");
+const generateTasksButton = document.getElementById("generate-tasks");
+const startButton = document.getElementById("start");
+const resetButton = document.getElementById("reset");
+const continuousCheckbox = document.getElementById("continuous");
 
 // Array to store tasks
 let tasks = [];
@@ -19,14 +21,15 @@ let continuousTimer;
 // Flags
 let stopExecution;
 
-
+// Counter for finished tasks
+let finishedTasks = 0;
 
 // Object to store algorithm functions
 const algorithms = {
-  fcfs: 'fcfsAlgorithm',
-  sjf: 'sjfAlgorithm',
-  prio: 'priorityAlgorithm',
-  rr: 'rrAlgorithm',
+  fcfs: "fcfsAlgorithm",
+  sjf: "sjfAlgorithm",
+  prio: "priorityAlgorithm",
+  rr: "rrAlgorithm",
 };
 
 // Function to generate a random number between min and max (inclusive)
@@ -34,54 +37,50 @@ function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 // Function to start the scheduling algorithm and update the statistics
 function startScheduling() {
   // Disable Input from changing
   disableStartButton();
   // Run the selected scheduling algorithm and update the statistics
   runAlgorithm().then(enableStartButton);
-  // Update the tasks container
-  //showTasks()
 }
 
 // Function to run the selected scheduling algorithm on the tasks array
 async function runAlgorithm() {
   // Get the selected scheduling algorithm
-  const selectedAlgorithm = algorithms[document.querySelector('input[name="algorithm"]:checked').value];
-  const timeToRun = document.querySelector('input[name="run-time"]').value
+  const selectedAlgorithm =
+    algorithms[document.querySelector('input[name="algorithm"]:checked').value];
+  const timeToRun = document.querySelector('input[name="run-time"]').value;
 
   // Run the selected algorithm on the tasks array
   switch (selectedAlgorithm) {
-    case 'fcfsAlgorithm':
+    case "fcfsAlgorithm":
       fcfs_sort();
       await executeTasks(timeToRun);
       break;
 
-    case 'sjfAlgorithm':
+    case "sjfAlgorithm":
       sjf_sort();
       await executeTasks(timeToRun);
       break;
 
-    case 'priorityAlgorithm':
+    case "priorityAlgorithm":
       priority_sort();
       await executeTasks(timeToRun);
       break;
 
-    case 'rrAlgorithm':
+    case "rrAlgorithm":
       rr_sort();
-      await executeTasks(timeToRun, document.querySelector('input[name="quantum"]').value);
+      await executeTasks(
+        timeToRun,
+        document.querySelector('input[name="quantum"]').value
+      );
       break;
 
     default:
       break;
   }
-
-
-  // Update the statistics container with the number of completed tasks, total time taken to complete the tasks, and average time taken per task
-   updateStatistics();
 }
-
 
 // First-Come, First-Serve (FCFS) algorithm
 function fcfs_sort() {
@@ -110,51 +109,61 @@ function rr_sort() {
 }
 
 // Function to execute tasks
-async function executeTasks(timeToRun,quantum=-1) {
+async function executeTasks(timeToRun, quantum = -1) {
   // Release blocking flag
   stopExecution = false;
   // Iterate through each task
-  for (let i = 0; tasks.length > 0 && timeToRun > 0 && !stopExecution; i=i%tasks.length) {
+  for (
+    let i = 0;
+    tasks.length > 0 && timeToRun > 0 && !stopExecution;
+    i = i % tasks.length
+  ) {
     const task = tasks[i];
     task.oldTime = task.timeRequired;
     [timeToRun, task.timeRequired, i] =
-        (quantum === -1) ?
-                    timeToRun >= task.timeRequired ? [timeToRun - task.timeRequired, 0, i] : [0, task.timeRequired - timeToRun, ++i] :
-                    quantum >= task.timeRequired ? [timeToRun - task.timeRequired, 0, i] : [timeToRun - quantum, task.timeRequired - quantum, ++i];
-    if (task.timeRequired === 0)tasks = tasks.filter(t => t !== task);
+      quantum === -1
+        ? timeToRun >= task.timeRequired
+          ? [timeToRun - task.timeRequired, 0, i]
+          : [0, task.timeRequired - timeToRun, ++i]
+        : quantum >= task.timeRequired
+        ? [timeToRun - task.timeRequired, 0, i]
+        : [timeToRun - quantum, task.timeRequired - quantum, ++i];
+    if (task.timeRequired === 0) tasks = tasks.filter((t) => t !== task);
     await animateTask(task);
-    }
   }
+}
 
 async function animateTask(task) {
   const taskDiv = document.getElementById(`task${task.id}`);
   taskDiv.runTime = taskDiv.querySelector("div.runTime");
-  taskDiv.classList.add('running');
-  while (task.oldTime !== task.timeRequired){
-    taskDiv.runTime.textContent= "Time left: " + --task.oldTime + 's';
+  taskDiv.classList.add("running");
+  while (task.oldTime !== task.timeRequired) {
+    taskDiv.runTime.textContent = "Time left: " + --task.oldTime + "s";
     await sleep(20);
   }
-  taskDiv.classList.remove('running');
+  taskDiv.classList.remove("running");
   if (task.timeRequired === 0) {
-    await finishTask(taskDiv)}
-    updateStatistics(statisticsContainer);
+    await finishTask(taskDiv);
+    finishedTasks += 1;
+  }
+  updateStatistics(statisticsContainer);
 }
 
 // Trigger Div remove animations
 function finishTask(taskDiv) {
-  taskDiv.classList.add('destroy-me');
-  return new Promise(resolve => {
-    taskDiv.addEventListener('animationend', () => {
+  taskDiv.classList.add("destroy-me");
+  return new Promise((resolve) => {
+    taskDiv.addEventListener("animationend", () => {
       taskDiv.parentNode.removeChild(taskDiv);
       resolve();
     });
   });
-  }
+}
 
 // Function to generate tasks
 async function generateTasks(numTasks = 1) {
   // Cancel if too much tasks already exist
-  if (continuousTimer && tasks.length > 50 )return;
+  if (continuousTimer && tasks.length > 50) return;
   // Generate a random number of tasks (between 1 and 20) if not input specified
   numTasks = numTasks === 1 ? numTasks : Math.floor(Math.random() * 20) + 1;
   // Generate tasks and add them to the tasks array and tasks container
@@ -166,7 +175,7 @@ async function generateTasks(numTasks = 1) {
       arrivalTime: Date.now(),
       startTime: null,
       endTime: null,
-      priority: Math.floor( Math.random() * 10) + 1,
+      priority: Math.floor(Math.random() * 10) + 1,
       timeRequired: Math.floor(Math.random() * 10) + 1,
     };
     tasks.push(task);
@@ -175,60 +184,44 @@ async function generateTasks(numTasks = 1) {
   }
 }
 
-// Function to update the statistics container with the results
-function updateStatistics() {
-  // Calculate the number of completed tasks
-  // to do: assign other value that will count the finished tasks 
-  const completedTasks = tasks.filter(task => task.remainingTime === 0).length;
-  // Calculate the total time taken to complete the tasks
-  const totalTime = tasks.reduce((total, task) => total + task.executionTime, 0);
-  // Calculate the average time taken per task
-  const averageTime = totalTime.toFixed(2) / tasks.length;
-  // Update the statistics container with the results
- // statisticsContainer.innerHTML = `Completed tasks: ${completedTasks} | Total time: ${totalTime} 's' | Average time per task: ${averageTime.toFixed(2)} 's' `;
-  document.querySelector(".num-completed .value").textContent = completedTasks;
-  document.querySelector(".total-time .value").textContent = totalTime;
-  document.querySelector(".avg-time .value").textContent = averageTime;
-}
-
-function showTasks(){
-  tasksContainer.innerHTML = '';
+function showTasks() {
+  tasksContainer.innerHTML = "";
   tasks.forEach(generateDiv);
 }
 
 // Function to generate div for a Task
 function generateDiv(task) {
-  const taskDiv = document.createElement('div');
-  taskDiv.classList.add('task');
+  const taskDiv = document.createElement("div");
+  taskDiv.classList.add("task");
   taskDiv.id = `task${task.id}`;
-  const arrivalTimeContainer = document.createElement('div');
-  arrivalTimeContainer.classList.add('arrivalTimeContainer');
+  const arrivalTimeContainer = document.createElement("div");
+  arrivalTimeContainer.classList.add("arrivalTimeContainer");
   arrivalTimeContainer.innerText = "Arrival Time : ";
-  const arrivalTime = document.createElement('span');
-  arrivalTime.classList.add('arrivalTime');
-  arrivalTime.innerText =  new Date(task.arrivalTime).toISOString();
+  const arrivalTime = document.createElement("span");
+  arrivalTime.classList.add("arrivalTime");
+  arrivalTime.innerText = new Date(task.arrivalTime).toISOString();
   arrivalTimeContainer.appendChild(arrivalTime);
-  const name = document.createElement('div');
-  name.classList.add('taskId');
+  const name = document.createElement("div");
+  name.classList.add("taskId");
   name.innerText = task.name;
-  const runTime = document.createElement('div');
-  runTime.classList.add('runTime');
-  runTime.innerText = "Duration: " + task.timeRequired + 's';
-  const priority = document.createElement('div');
-  priority.classList.add('priority');
+  const runTime = document.createElement("div");
+  runTime.classList.add("runTime");
+  runTime.innerText = "Duration: " + task.timeRequired + "s";
+  const priority = document.createElement("div");
+  priority.classList.add("priority");
   priority.innerText = "Priority: " + task.priority;
   taskDiv.append(...[name, arrivalTimeContainer, runTime, priority]);
   tasksContainer.appendChild(taskDiv);
 }
 
 // Event listener for the generate tasks button
-generateTasksButton.addEventListener('click', generateTasks);
+generateTasksButton.addEventListener("click", generateTasks);
 
 // Event listener for the start button
-startButton.addEventListener('click', startScheduling);
+startButton.addEventListener("click", startScheduling);
 
 // Event listener for the continuous checkbox
-continuousCheckbox.addEventListener('change', function() {
+continuousCheckbox.addEventListener("change", function () {
   // Clear any existing timer
   clearInterval(continuousTimer);
   // If the checkbox is checked, start a timer to continuously generate new tasks
@@ -238,37 +231,58 @@ continuousCheckbox.addEventListener('change', function() {
 });
 
 // Event listener for the reset button
-resetButton.addEventListener('click', function() {
-  // Reset the taskIds and update the nextTaskId variable
+resetButton.addEventListener("click", function () {
+  // Reset the values
+  finishedTasks = 0;
   tasks = [];
   taskIdCounter = 0;
-  tasksContainer.innerHTML = '';
+  tasksContainer.innerHTML = "";
+
   // Reset the statistics container
-  statisticsContainer.innerHTML = '';
+  document.getElementById("finished-tasks").textContent = "0";
+  statisticsContainer.classList.remove("hidden");
   enableStartButton();
+  updateStatistics();
 });
 
-// Event listener for selecting algorithms
+
+// Function to update the statistics
+function updateStatistics(statisticsContainer) {
+  // Clear the statistics container
+  statisticsContainer.innerHTML = "";
+  // Create new statistics elements
+  const numTasksElement = document.createElement("p");
+  numTasksElement.textContent = "Number of not started tasks: " + tasks.length;
+  numTasksElement.classList.add("bold");
+  const finishedTasksElement = document.createElement("p");
+  finishedTasksElement.textContent = "Number of finished tasks: " + finishedTasks;
+  finishedTasksElement.classList.add("bold");
+     
+ // Add the new statistics elements to the statistics container
+  statisticsContainer.appendChild(numTasksElement);
+  statisticsContainer.appendChild(finishedTasksElement);
+ }
+
+ // Event listener for selecting algorithms
 algorithmsContainer.forEach((radioButton) => {
-  radioButton.addEventListener('change', () => {
+  radioButton.addEventListener("change", () => {
     // Stop the scheduling function here
     stopExecution = true;
   });
 });
 
 function sleep(duration) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve();
     }, duration);
   });
 }
 
-
 function disableStartButton() {
-  startButton.setAttribute('disabled', true);
+  startButton.setAttribute("disabled", true);
 }
 
 function enableStartButton() {
-  startButton.removeAttribute('disabled');
+  startButton.removeAttribute("disabled");
 }
